c06cebaa6732250d26bd4d7f718ccfcb6132bd44
Никита Сотников <sotnikovnikita4@gmail.com>
2026-01-29 20:02:51 +0300

    openapi

--- src/main/java/ru/itupolev/hackaton/config/SecurityConfig.java
+++ src/main/java/ru/itupolev/hackaton/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,7 +19,9 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/register").permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults());
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(m ->
+                        m.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
